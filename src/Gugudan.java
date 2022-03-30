61b1e20ae831b9ba0ddd2337e78652ac47806adf
MASTER <MASTER@MY_COM>
2022-03-30 11:19:57 +0900

    메소드화 하여 하나로 합침

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,103 +2,29 @@ import java.util.Scanner;
 
 
 public class Gugudan {
-	public static void main (String[] args) {
-//		//2단
-//		System.out.println("2단");
-//		System.out.println(2*1);
-//		System.out.println(2*2);
-//		System.out.println(2*3);
-//		System.out.println(2*4);
-//		System.out.println(2*5);
-//		System.out.println(2*6);
-//		System.out.println(2*7);
-//		System.out.println(2*8);
-//		System.out.println(2*9);
-//		//3단
-//		System.out.println("3단");
-//		System.out.println(3*1);
-//		System.out.println(3*2);
-//		System.out.println(3*3);
-//		System.out.println(3*4);
-//		System.out.println(3*5);
-//		System.out.println(3*6);
-//		System.out.println(3*7);
-//		System.out.println(3*8);
-//		System.out.println(3*9);
-//		//4단
-//		System.out.println("4단");
-//		int result = 4*1;
-//		System.out.println("4*1="+result);
-//		result = 4*2;
-//		System.out.println("4*2="+result);
-//		result = 4*3;
-//		System.out.println("4*3="+result);
-//		result = 4*4;
-//		System.out.println("4*4="+result);
-//		result = 4*5;
-//		System.out.println("4*5="+result);
-//		result = 4*6;
-//		System.out.println("4*6="+result);
-//		result = 4*7;
-//		System.out.println("4*7="+result);
-//		result = 4*8;
-//		System.out.println("4*8="+result);
-//		result = 4*9;
-//		System.out.println("4*9="+result);
-//		
-//		//변수입력 후 출력하는 프로그램
-//		System.out.println("출력할 구구단은? :");
-//		Scanner scanner = new Scanner(System.in);
-//		int number = scanner.nextInt();
-//		System.out.println(number+"*1="+number*1);
-//		System.out.println(number+"*2="+number*2);
-//		System.out.println(number+"*3="+number*3);
-//		System.out.println(number+"*4="+number*4);
-//		System.out.println(number+"*5="+number*5);
-//		System.out.println(number+"*6="+number*6);
-//		System.out.println(number+"*7="+number*7);
-//		System.out.println(number+"*8="+number*8);
-//		System.out.println(number+"*9="+number*9);
-//		
-//		int i=1;
-//		while (i < 10) {
-//            // 곱셈 결과 출력
-//        	System.out.println(6+"*"+i+"="+6*i);
-//            // i 값을 1 증가
-//        	i+=1;
-//        }
-//		
-//		for(int j =1; j<100;j++) {
-//			System.out.println(7+"*"+j+"="+7*j);
-//		}
-		
-		//if 문
-		
-		System.out.println("출력할 구구단을 말해주세요:");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("사용자가 입력한값: "+number);
-		
-		if (number < 2) {
-			System.out.println("Error : number is too small");
-		} else if (number > 9) {
-			System.out.println("Error : number is too big");
-		} else {
-			for (int i =1; i<10; i++) {
-				System.out.println(number * i);
-			}
+	//메소드
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
+		for (int i=0; i<result.length; i++) {
+			result[i]= times * (i+1);
 		}
+		return result;
+	}
+	
+	public static void print(int[] result) {
+		for(int i =0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main (String[] args) {
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		for(int i=2; i<10; i++) {
+			System.out.println("Gugudan : " + i);
+			int[] result = calculate(i);
+			print(result);
+		}
 		
 		
 	}
